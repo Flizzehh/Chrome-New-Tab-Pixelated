b7eda92142caf8d55700437bcd4b17d178b61e84
Ryan White <ryan.thomas.white@outlook.com>
2017-11-08 02:27:46 -0500

    Cleaned up the javascript of all old code.

--- control.js
+++ control.js
@@ -1,25 +1,11 @@
 var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 function calcColours(timeSum) {
-	//var rVal = 1 * (((255 / 8) * Math.sin(2 * Math.PI * (timeSum / (86400 * 12)) * 255) + (255 / 4)) * (1 / Math.sinh(Math.PI * (timeSum / 86400) + 1)));
-	//var gVal = 1 * (((255 / 8) * Math.sin(2 * Math.PI * ((86400 - timeSum) / (86400 * 12)) * 255) + (255 / 4)) * (1 / Math.sinh(Math.PI * ((86400 - timeSum) / 86400) + 1)));
-	//var bVal = 0.5 * ((Math.pow((timeSum - (86400 / 2)) / (86400 / 20), 2)) + 80 * Math.sin(timeSum / (864*2)) + 40) + 150;
-
-	//rVal += 16 + 54;
-	//gVal += 16 + 54;
-	//bVal += 0;
-
-	//var rVal = ((timeSum / 86400) - (1/3)) * 255;
-	//var gVal = ((timeSum / 86400) - (2/3)) * 255;
-	//var bVal = ((timeSum / 86400) - (3/3)) * 255;
-
 	var td = new Date();
 
 	var dayOffset = (((td.getTime() / 1000 / 60 / 60 / 24) * 3000) % 86400) / 86400;
-	var normalizedTimeSum = ((timeSum) / 86400) + dayOffset;// + (((td.getUTCDate() + dayOffset) * 2000) / 86400);
+	var normalizedTimeSum = ((timeSum) / 86400) + dayOffset;
 
-	//var rVal = 255 * (-2 * Math.pow(normalizedTimeSum - 0.5,2) + 1);
-	//var gVal = 255 * (2 * Math.pow(normalizedTimeSum - 0.5,2));
 	var rVal = 255 * (0.25 * Math.sin(2 * Math.PI * normalizedTimeSum) + 0.5);
 	var gVal = 255 * (0.25 * Math.sin(4 * Math.PI * normalizedTimeSum) + 0.5);
 	var bVal = 255 * (0.25 * Math.sin(6 * Math.PI * normalizedTimeSum) + 0.5);
@@ -81,7 +67,6 @@ function calculateTimeSum(hours, minutes, seconds) {
 
 function checkTime() {
 	var timeSum = 0;
-	//switchTextColour();
 	window.setInterval(function () {
 		var td = new Date();
 		var hours = td.getHours();
